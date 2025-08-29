67e008a308ca6081b7918afa38237e8e5a80b3e0
Arnil <Andersgrangaard@gmail.com>
2025-08-29 08:39:02 +0200

    gitignore

---
+++ Program.cs
@@ -0,0 +1,7 @@
+if (args[0] == "say")
+{
+    var message = args[1];
+    var frequency = int.Parse(args[2]);
+    foreach (var i in Enumerable.Range(1, frequency))
+        Console.Write(message + " ");
+}
